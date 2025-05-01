217eb325e243d191b57dd1033655d38ad14e4852
user <abdugafforlutfillaev14@gmail.com>
2025-05-01 19:34:38 +0300

    feat: add switch to header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,12 @@
-import { Group, Text } from '@mantine/core';
+'use client';
+import { Group, Switch, Text } from '@mantine/core';
 import { IconShoppingCartFilled } from '@tabler/icons-react';
 import React from 'react';
 import { NavButtons } from './NavButtons';
+import { useToggle } from '@mantine/hooks';
 
 const Header = () => {
+  const [value, toggle] = useToggle(['Postgresql', 'Mysql']);
   return (
     <header className="flex justify-between items-center pl-32 pr-10 py-4 bg-[#4169e2] sticky top-0 z-10">
       <Group gap="14px" align="center">
@@ -12,7 +15,18 @@ const Header = () => {
           ShopDB
         </Text>
       </Group>
-      <NavButtons />
+
+      <Group>
+        <Switch
+          ml={50}
+          size="lg"
+          checked={value === 'Postgresql'}
+          onChange={() => toggle()}
+          onLabel={<Text p={10} fz={12}>Postgresql</Text>}
+          offLabel={<Text p={10} fz={12}>MySql</Text>}
+        />
+        <NavButtons />
+      </Group>
     </header>
   );
 };
